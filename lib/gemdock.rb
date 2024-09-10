c51abcc816eb2a41ad4d692ce2e09a63771d2ad6
Saiqul Haq <saiqulhaq@gmail.com>
2024-09-11 05:53:10 +0700

    bundle gem gemdock

---
+++ lib/gemdock.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "gemdock/version"
+
+module Gemdock
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/gemdock/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Gemdock
+  VERSION = "0.1.0"
+end
